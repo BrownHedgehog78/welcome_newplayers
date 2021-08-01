82898cf3a74714ce64374c1029a34eb73ba94c23
Toby1710 <69259430+DiamondPlane@users.noreply.github.com>
2021-08-01 11:39:14 +0300

    Change welcome message position to HUD

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Minetest mod that sends a welcome message to new players
 
 ![image](https://user-images.githubusercontent.com/69259430/127742159-c9b7f6f9-dd9b-4803-9a75-73240e85cd42.png)
 #
-You can edit the welcome messages, use /welcome_msg, it will show a formspec where you can edit.
+You can edit the welcome messages or change the welcome message position (by default it's in HUD), use /welcome_msg, it will show a formspec where you can edit.
--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local welcome_msg = minetest.settings:get("welcome_newplayers.msg") or "Welcome to the server!"
+local welcome_pos = minetest.settings:get("welcome_newplayers.msg_pos") or "hud"
 
 minetest.register_chatcommand("welcome_msg", {
 	description = "Edit welcome message",
@@ -6,12 +7,15 @@ minetest.register_chatcommand("welcome_msg", {
 	func = function(name)
 		local formspec = {
 			"formspec_version[4]" ..
-			"size[9,5]" ..
+			"size[9,7]" ..
 			"label[2.1,0.6;Welcome Messages Editor]" ..
-			"label[2.4,1.7;Edit Welcome Message]" ..
-			"field[0.2,2;8.5,0.8;welcome_msg_field;;]" ..
-			"button[4.7,3.7;3,0.9;save_button;Save]" ..
-			"button_exit[0.7,3.7;3,0.9;cancel_button_exit;Cancel]"
+			"label[0.8,1.8;Change Welcome Message Position (optional)]" ..
+			"button[0.8,2.4;3,0.9;welcome_msg_chat_button;Chat]" ..
+			"button[4.6,2.4;3,0.9;welcome_msg_hud_button;HUD]" ..
+			"label[2.4,4.1;Edit Welcome Message]" ..
+			"field[0.2,4.5;8.5,0.8;welcome_msg_field;;]" ..
+			"button[4.7,5.7;3,0.9;save_button;Save]" ..
+			"button_exit[0.7,5.7;3,0.9;cancel_button_exit;Cancel]"
 		}
 		minetest.show_formspec(name, "welcome_newplayers:editor", table.concat(formspec, ""))
 	end
@@ -20,22 +24,41 @@ minetest.register_chatcommand("welcome_msg", {
 minetest.register_on_newplayer(function(player)
 	local name = player:get_player_name()
 	minetest.after(2.5, function()
-		minetest.chat_send_player(name, minetest.colorize("#4bd12a", welcome_msg))
+		if welcome_format == "hud" then
+			welcome_hud = player:hud_add({
+				hud_elem_type = "text",
+				position = {x = 0.5, y = 0.5},
+				text = welcome_msg,
+				number = 0x4bd12a,
+				size = {x = 3, y = 3}
+			})
+			minetest.after(3, function()
+				player:hud_remove(welcome_hud)
+			end)
+		elseif welcome_pos == "chat" then
+			minetest.chat_send_player(name, minetest.colorize("#4bd12a", welcome_msg))
+		end
 	end)
 end)
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
 	local name = player:get_player_name()
 	if formname == "welcome_newplayers:editor" then
-		if not fields.save_button then
-			return
+		if fields.welcome_msg_chat_button then
+			minetest.settings:set("welcome_newplayers.msg_pos", "chat")
+			minetest.chat_send_player(name, "Changed welcome message position to chat (the server will need restart)")
+		elseif fields.welcome_msg_hud_button then
+			minetest.settings:set("welcome_newplayers.msg_pos", "hud")
+			minetest.chat_send_player(name, "Changed welcome message position to HUD (the server will need restart)")
 		end
 
-		if fields.welcome_msg_field == ""  then
-			minetest.chat_send_player(name, "Nothing to edit, the field is empty")
-		else
-			minetest.settings:set("welcome_newplayers.msg", fields.welcome_msg_field)
-			minetest.chat_send_player(name, "Edited welcome message! (The server will need restart)")
+		if fields.save_button then
+			if fields.welcome_msg_field == ""  then
+				minetest.chat_send_player(name, "Nothing to edit, the field is empty")
+			else
+				minetest.settings:set("welcome_newplayers.msg", fields.welcome_msg_field)
+				minetest.chat_send_player(name, "Edited welcome message! (The server will need restart)")
+			end
 		end
 	end
 end)
